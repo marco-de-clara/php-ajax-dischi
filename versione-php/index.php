0260c15ea97ab2153c2373aade98dcbf1b6637ed
Marco De Clara <marcodeclara@gmail.com>
2020-06-18 00:24:47 +0200

    Base structure

---
+++ versione-php/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Dischi Musicali</title>
+        <link rel="stylesheet" href="public/css/app.css">
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <form>
+                    <label for="genre">Choose a genre:</label>
+                    <select name="genre" id="genre">
+                        <option value="all">All</option>
+                        <option value="pop">Pop</option>
+                        <option value="rock">Rock</option>
+                        <option value="metal">Metal</option>
+                        <option value="jazz">Jazz</option>
+                    </select>
+                  </form>
+            </div>
+        </header>
+        
+        <script src="public/js/app.js" charset="UTF-8"></script>
+    </body>
+</html>
